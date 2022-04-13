c1ec73f29618ffd1ffe2e0430a5e9356a41826a7
Pfarr <pjamespy4@gmail.com>
2022-04-13 13:54:24 -0700

    created parent enemy script and ghost script

--- Assets/Scripts/Enemies/Enemy.cs
+++ Assets/Scripts/Enemies/Enemy.cs
@@ -2,17 +2,50 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Enemy
+public class Enemy : MonoBehaviour
 {
+    public int rank;
+
     protected int _magicPoints;
     protected int _shootingPoints;
     protected int _fightPoints;
     protected int _generatorPoints;
 
-    protected float _damage;
+    protected int _damage;
     protected int _hitPoints;
 
     public bool isMagicDamagable;
     public bool isShootDamagable;
     public bool isFightDamagable;
+
+    private float _moveSpeed = .005f;
+    public int speed = 1;
+
+    void Update()
+    {
+        CheckPlayerPos();
+        Debug.Log("Checking Player Pos");
+    }
+
+    private void CheckPlayerPos()
+    {
+        Collider[] hitColliders = Physics.OverlapSphere(gameObject.transform.position, 10);
+        if (hitColliders.Length > 0)
+        {
+            foreach (Collider hitCollider in hitColliders)
+            {
+                if (hitCollider.tag == "Player")
+                {
+                    FollowPlayer(hitCollider.gameObject);
+                    Debug.Log("Player In Collider");
+                }
+            }
+        }
+    }
+
+    private void FollowPlayer(GameObject player)
+    {
+        transform.position = Vector3.MoveTowards(transform.position, player.transform.position, speed * _moveSpeed);
+        Debug.Log("Moving To Player");
+    }
 }
