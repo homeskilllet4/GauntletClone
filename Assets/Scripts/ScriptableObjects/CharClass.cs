6d3923601c4114fa1d33bfd423333b0f4999823b
Kade Chambers <kgbchambers@gmail.com>
2022-04-13 19:08:57 -0700

    CharClass Scriptable Objects
    Character class scriptable objects created

---
+++ Assets/Scripts/ScriptableObjects/CharClass.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Character Class", menuName = "ScriptableObjects/CharacterClass", order = 1)]
+public class CharClass : ScriptableObject
+{
+    public string className;
+    [Range(1, 3), Tooltip("Ranged Power: 1=weak, 2=moderate, 3=strong")]
+    public int magicPower;
+    [Range(1, 3), Tooltip("Melee Power: 1=Weak, 2=moderate, 3=strong")]
+    public int fightPower;
+    [Range(1,3), Tooltip("Player speed: 1=slow, 2=moderate, 3=fast")]
+    public int movementSpeed;
+    [Range(1, 3), Tooltip("Shot speed: 1=slow, 2=moderate, 3=fast")]
+    public int shotSpeed;
+    [Range(0, 3), Tooltip("Character Armor: 1=none, 2=moderate, 3=fast")]
+    public int armor;
+}
