2c7c089359d58692df08b91040b8860b664e598f
homeskilllet4 <pjamespy4@gmail.com>
2022-04-14 11:05:35 -0700

    implemented object pooling and worked on the demon script. fixed the ability for enemies to go through walls and they will now track when the boundary is between the player and the enemy, but will not follow once they hit the barrier unless the player goes around it.

--- Assets/Scripts/Enemies/Enemy.cs
+++ Assets/Scripts/Enemies/Enemy.cs
@@ -6,28 +6,34 @@ public class Enemy : MonoBehaviour
 {
     public int rank;
 
-    protected int _magicPoints;
-    protected int _shootingPoints;
-    protected int _fightPoints;
-    protected int _generatorPoints;
+    protected int _magicPoints; //points awarded for using magic on this enemy
+    protected int _shootingPoints; //points awarded for shooting this enemy
+    protected int _fightPoints; //points awarded for fighting this enemy
+    protected int _generatorPoints; //points awarded for destroying generator
 
-    protected int _damage;
-    protected int _hitPoints;
+    protected int _damage; //damage done to player
+    protected int _hitPoints; //amount of life
 
-    public bool isMagicDamagable;
-    public bool isShootDamagable;
-    public bool isFightDamagable;
+    public bool isMagicDamagable; //is damagable by magic
+    public bool isShootDamagable; //is damagable by shooting
+    public bool isFightDamagable; //is damagable by fighting
 
-    private float _moveSpeed = .005f;
-    public int speed = 1;
+    private float _moveSpeed = .05f; //base speed
+    public int speed = 1; //speed multiplier
 
-    void Update()
+    public bool _isOnBlockade = false; //is blockade between the player and enemy
+    public bool _isTouchingBlockade; //is it touching blockade?
+    //public bool _isMoving; //is this enemy moving
+    private RaycastHit hit;
+
+    void FixedUpdate()
     {
+        //check where the player is and follow them
         CheckPlayerPos();
         Debug.Log("Checking Player Pos");
     }
 
-    private void CheckPlayerPos()
+    protected void CheckPlayerPos()
     {
         Collider[] hitColliders = Physics.OverlapSphere(gameObject.transform.position, 10);
         if (hitColliders.Length > 0)
@@ -36,6 +42,7 @@ public class Enemy : MonoBehaviour
             {
                 if (hitCollider.tag == "Player")
                 {
+                    //follow the player
                     FollowPlayer(hitCollider.gameObject);
                     Debug.Log("Player In Collider");
                 }
@@ -45,8 +52,29 @@ public class Enemy : MonoBehaviour
 
     private void FollowPlayer(GameObject player)
     {
-        transform.position = Vector3.MoveTowards(transform.position, player.transform.position, speed * _moveSpeed);
-        Debug.Log("Moving To Player");
+        //move towards the player
+        if (Physics.Linecast(transform.position, player.transform.position, out hit))
+        {
+            if (hit.transform.tag == "Blockade")
+            {
+                if (_isTouchingBlockade)
+                {
+                    _isOnBlockade = true;
+                    Debug.Log("Blockade in the way");
+                }
+            }
+            else
+            {
+                _isOnBlockade = false;
+                _isTouchingBlockade = false;
+            }
+        }
+
+        if (_isOnBlockade != true)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, player.transform.position, speed * _moveSpeed);
+            Debug.Log("Moving To Player");
+        }
     }
 
     protected void CheckLives()
@@ -60,5 +88,14 @@ public class Enemy : MonoBehaviour
     public void Disable()
     {
         gameObject.SetActive(false);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Blockade"))
+        {
+            _isTouchingBlockade = true;
+            Debug.Log("Touching blockade");
+        }
     }
 }
---
+++ Assets/Scripts/ObjectPooler.cs
@@ -0,0 +1,80 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class ObjectPoolItem
+{
+    public GameObject objectToPool;
+    public int amountToPool;
+    public bool shouldExpand = true;
+}
+
+public class ObjectPooler : MonoBehaviour
+{
+    public List<GameObject> pooledObjects;
+    public List<ObjectPoolItem> itemsToPool;
+
+    private static ObjectPooler _instance;
+
+    public static ObjectPooler Instance { get { return _instance; } }
+
+    private void Awake()
+    {
+        if (_instance != null && _instance != this)
+        {
+            Destroy(this.gameObject);
+        }
+        else
+        {
+            _instance = this;
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        pooledObjects = new List<GameObject>();
+        foreach (ObjectPoolItem item in itemsToPool)
+        {
+            for (int i = 0; i < item.amountToPool; i++)
+            {
+                GameObject obj = (GameObject)Instantiate(item.objectToPool);
+                obj.SetActive(false);
+                pooledObjects.Add(obj);
+            }
+        }
+    }
+
+    public GameObject GetPooledObject(string tag)
+    {
+        //loop through the list of pooledObjects
+        //check to see if the item in our list is not currently active
+        //if it is, move to the next in the list
+        //else you need to exit the function and hand the inactive object 
+        //to the function called GetPooledObject
+        for (int i = 0; i < pooledObjects.Count; i++)
+        {
+            if (!pooledObjects[i].activeInHierarchy && pooledObjects[i].tag == tag)
+            {
+                return pooledObjects[i];
+            }
+        }
+
+        foreach (ObjectPoolItem item in itemsToPool)
+        {
+            if (item.objectToPool.tag == tag)
+            {
+                if (item.shouldExpand)
+                {
+                    GameObject obj = (GameObject)Instantiate(item.objectToPool);
+                    obj.SetActive(false);
+                    pooledObjects.Add(obj);
+                    return obj;
+                }
+            }
+        }
+
+        return null;
+    }
+}
